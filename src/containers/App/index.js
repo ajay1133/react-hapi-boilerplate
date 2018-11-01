0d45f962ba1d69661e997f02ed5823ec20964195
Monty Khanna <monty.khanna@simsaw.com>
2018-11-01 15:51:36 +0530

    config setup

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Segment, Sidebar, Grid } from 'semantic-ui-react'
-import {ConnectedRouter} from 'connected-react-router/immutable'
+import { ConnectedRouter } from 'connected-react-router/immutable'
 import { connect } from 'react-redux';
 import NavBar from '../../components/NavBar'
 import routes from '../../routes'
@@ -10,36 +10,36 @@ import 'semantic-ui-css/semantic.css'
 import '../../style/css/style.css';
 
 class App extends Component {
-    render () {
-        const { history } = this.props;
-        return (
-        <div className="full-height">
-            <Sidebar.Pushable as={Segment}>
-            <Sidebar.Pusher>
-                <Segment basic>
-                {
-                    isShow &&
-                    <Grid>
-                    <Grid.Row>
-                        <Grid.Column width={16} className="pt-20 pb-30">
-                            <NavBar isShow={isShow}/>
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
-                }              
-                <ConnectedRouter history={history}>{ routes }</ConnectedRouter>
-                </Segment>
-            </Sidebar.Pusher>
-            </Sidebar.Pushable>
-        </div>
-        );
-    }
+  render () {
+    const { history, isShow } = this.props;
+    return (
+      <div className="full-height">
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar.Pusher>
+            <Segment basic>
+              {
+                isShow &&
+                <Grid>
+                  <Grid.Row>
+                    <Grid.Column width={16} className="pt-20 pb-30">
+                      <NavBar isShow={isShow} />
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+              }
+              <ConnectedRouter history={history}>{ routes }</ConnectedRouter>
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </div>
+    );
+  }
 }
 App.propTypes = {
-    history: PropTypes.object,
-    user: PropTypes.object
+  history: PropTypes.object,
+  user: PropTypes.object
 };
 
 export default connect(state => ({
-    user: state.get('auth').get('user')
+  user: state.get('auth').get('user')
 }))(App);
