5419ae1dc67d7fff849f4f931c0636fc3f89be9a
Monty Khanna <monty.khanna@simsaw.com>
2019-01-15 16:04:42 +0530

    Added: contactUs page

--- api/controllers/contactUs/listContactUs.js
+++ api/controllers/contactUs/listContactUs.js
@@ -1,3 +1,4 @@
+const joi = require('joi');
 const boom = require('boom');
 const contactService = require('../../services/contactService');
 
@@ -18,9 +19,30 @@ module.exports = {
   
   notes: 'Get all contacts',
   
+  validate: {
+    query: {
+      Page: joi.number()
+               .description('page number to get list of contacts'),
+      
+      Limit: joi.number()
+                .description('number of contacts per page'),
+      
+      SortBy: joi.string()
+                 .allow('')
+                 .description('field name'),
+      
+      Order: joi.string()
+                .allow('')
+                .description('asc or desc'),
+      
+    },
+    options: { abortEarly: false }
+  },
   handler: async (request, h) => {
+    const { query } = request;
+    
     try {
-      let data = await contactService.getContactUs();
+      let data = await contactService.getContactUs(query);
       return h.response(data);
     } catch(err) {
       return boom.badRequest(err);
