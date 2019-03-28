d4fa9208ee59bcd995364a889fc1e6d93c1b9ee4
unknown <ajay.nitk2007@gmail.com>
2019-03-28 12:47:27 +0530

    Removed icons and unwanted code

--- api/db/seeders/20180912111825-createAdminUser.js
+++ api/db/seeders/20180912111825-createAdminUser.js
@@ -2,7 +2,7 @@ const accountService = require('../../services/accountService');
 module.exports = {
   up: async () => {
     const adminUser = {
-      email: `admin@shareCabs.com`,
+      email: `admin@premierRecruiter.com`,
       password: "admin123",
       role: 1,
       firstName: "Super",
